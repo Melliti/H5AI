85290b7d7a7086d6389deaa0cb1522fe360bc070
Melliti <r.melliti@protonmail.com>
2019-08-07 10:54:50 +0200

    [Add] icons for files [EDIT] Only folders are clickable

--- layouts/files.php
+++ layouts/files.php
@@ -1,9 +1,23 @@
 <?php
 
-function displayDir($folders) {
+function defineIcon($handle) {
+    $ext = substr($handle, strrpos($handle, '.') + 1);
+    $icon = "http://localhost/H5AI/icons/" . $ext . ".png" ;
+    echo "<p><img src=\"$icon\" alt=\"language icon\"/> ";    
+}
+
+function displayDirContent($folders) {
     $baseURL = "http://localhost" . $_SERVER['REQUEST_URI']; 
     while ($handle = readdir($folders)) {
         $url = $baseURL . "/" . $handle;
-        echo "<a href=\"{$url}\">{$handle}</a><br>";
+        if ($handle != "." && $handle != "..")
+        {
+            if (strpos($handle, "."))
+                defineIcon($handle);
+            if (is_dir($handle))
+                echo "<a href=\"{$url}\">{$handle}</a></p>";
+            else
+            echo "{$handle}</p>";
+        }   
     }
 }
