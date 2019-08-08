011c2d1a502350b2dd2b435612d5a7489f6a4818
Melliti <r.melliti@protonmail.com>
2019-08-08 23:53:28 +0200

    [Refacto] files.php in poo [Add] file size, file last update

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     include_once('layouts/nav.php');
     include_once('layouts/files.php');
     $explorer = new Explorer;
+    $DirectoryContent = new Files;
+
     $folder = $explorer->getURL();
     $resource = $explorer->getResource();
 
     displayNav($folder);
-    displayDirContent($resource, $explorer->getPath());
+    $DirectoryContent->listDirContent($resource, $explorer->getPath());
 ?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- layouts/files.php
+++ layouts/files.php
@@ -1,29 +1,55 @@
 <?php
 
-function defineIcon($handle, $path) {
-    $ext = substr($handle, strrpos($handle, '.') + 1);
-    $icon = "http://localhost/H5AI/icons/" . $ext . ".png" ;
-    if (file_exists("./icons/" . $ext . ".png"))
-        {
-            echo "<p><img src=\"$icon\" alt=\"language icon\"/> ";
-        }
-    else if (is_dir($path . '/' . $handle))
-        echo "<p><img src=\"http://localhost/H5AI/icons/dir.png\" alt=\"language icon\"/>" ;    
-    else
-        echo "<p><img src=\"http://localhost/H5AI/icons/unknown.png\" alt=\"language icon\"/>" ;
-}
+class Files {
+    private $audioTypes = ['mp3', 'wac', 'flac', 'wav'];
+    private $imgTypes = ['jpg', 'png', 'gif', 'bmp', 'svg'];
+    private $fileInfo= [];
 
-function displayDirContent($folders, $path) {
-    $baseURL = "http://localhost" . $_SERVER['REQUEST_URI']; 
-    while ($handle = readdir($folders)) {
-        $url = $baseURL . "/" . $handle;
-        if ($handle != "." && $handle != "..")
-        {
-            defineIcon($handle, $path);
-            if (is_dir($path . "/" . $handle))
-                echo "<a href=\"{$url}\">{$handle}</a></p>";
-            else
-            echo "{$handle}</p>";
-        }   
+    // Show file details
+    private function displayFileInfo($url) {
+        echo "<p><img src=\"{$this->fileInfo["icon"]}\" alt=\"language icon\"/>";
+        echo "<a href=\"{$url}\"> {$this->fileInfo["filename"]}</a></p>";
+        echo date('d/m/y', $this->fileInfo["mtime"]) . " - ";
+        echo number_format($this->fileInfo["size"] / 1024, 2) . ' KB';
+    }
+
+    // Get data about the file
+    private function file_information($path) {
+        $data = stat($path);
+        $this->fileInfo['size'] = $data['size'];
+        $this->fileInfo['mtime'] = $data['mtime'];
+    }
+
+    // Select the expected icon for each file
+    private function defineIcon($handle, $path) {
+        $ext = substr($handle, strrpos($handle, '.') + 1);
+        $icon = "http://localhost/H5AI/icons/" . $ext . ".png" ;
+        if (file_exists("./icons/" . $ext . ".png"))
+            $this->fileInfo['icon'] = $icon;
+        else if (is_dir($path . '/' . $handle))
+            $this->fileInfo['icon'] = "http://localhost/H5AI/icons/dir.png";
+        else if (in_array($ext, $this->audioTypes))
+            $this->fileInfo['icon'] = "http://localhost/H5AI/icons/audio.png";
+        else if (in_array($ext, $this->imgTypes))
+            $this->fileInfo['icon'] = "http://localhost/H5AI/icons/img.png";
+        else
+            $this->fileInfo['icon'] = "http://localhost/H5AI/icons/unknown.png";
+    }
+
+    // List content of a dir and send it to display function
+    public function listDirContent($folders, $path) {
+        $baseURL = "http://localhost" . $_SERVER['REQUEST_URI']; 
+        
+        while ($handle = readdir($folders)) {
+            $fullPath = $path . "/" . $handle;
+            $url = $baseURL . "/" . $handle;
+            $this->file_information($fullPath);
+            $this->fileInfo['filename'] = $handle;
+            if ($handle != "." && $handle != "..")
+            {
+                $this->defineIcon($handle, $path);
+                $this->displayFileInfo($url);
+            }  
+        }
     }
 }
