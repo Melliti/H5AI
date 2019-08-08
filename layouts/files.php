a5aa5cd663151915ccd735a01f28607d03ead8df
Melliti <r.melliti@protonmail.com>
2019-08-08 15:34:37 +0200

    [Add] icons for programmig language, unknown files, folders

--- layouts/files.php
+++ layouts/files.php
@@ -1,20 +1,26 @@
 <?php
 
-function defineIcon($handle) {
+function defineIcon($handle, $path) {
     $ext = substr($handle, strrpos($handle, '.') + 1);
     $icon = "http://localhost/H5AI/icons/" . $ext . ".png" ;
-    echo "<p><img src=\"$icon\" alt=\"language icon\"/> ";    
+    if (file_exists("./icons/" . $ext . ".png"))
+        {
+            echo "<p><img src=\"$icon\" alt=\"language icon\"/> ";
+        }
+    else if (is_dir($path . '/' . $handle))
+        echo "<p><img src=\"http://localhost/H5AI/icons/dir.png\" alt=\"language icon\"/>" ;    
+    else
+        echo "<p><img src=\"http://localhost/H5AI/icons/unknown.png\" alt=\"language icon\"/>" ;
 }
 
-function displayDirContent($folders) {
+function displayDirContent($folders, $path) {
     $baseURL = "http://localhost" . $_SERVER['REQUEST_URI']; 
     while ($handle = readdir($folders)) {
         $url = $baseURL . "/" . $handle;
         if ($handle != "." && $handle != "..")
         {
-            if (strpos($handle, "."))
-                defineIcon($handle);
-            if (is_dir($handle))
+            defineIcon($handle, $path);
+            if (is_dir($path . "/" . $handle))
                 echo "<a href=\"{$url}\">{$handle}</a></p>";
             else
             echo "{$handle}</p>";
--- layouts/nav.php
+++ layouts/nav.php
@@ -1,9 +1,10 @@
 <?php
 
 function displayNav($folders) {
-    $baseURL = "http://localhost/H5AI"; 
+    $baseURL = "http://localhost/H5AI";
+    echo "<a href=\"{$baseURL}\">Home</a> ";
     foreach ($folders as $elem) {
         $baseURL = $baseURL . "/" . $elem;
-        echo "<a href=\"{$baseURL}\">{$elem}</a> > ";        
+        echo "<a href=\"{$baseURL}\">{$elem}</a> > ";
     }
 }
